a6f1714454155d1bb80c797a28e8afd444a2d22f
timothyndiritu <timmyndiritu@gmail.com>
2024-12-04 10:44:26 +0300

    changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8cWrgtvstRUN9V7pSAE0P95-bZSWKsSQ",
-  authDomain: "rsvp-system-eff0c.firebaseapp.com",
-  projectId: "rsvp-system-eff0c",
-  storageBucket: "rsvp-system-eff0c.firebasestorage.app",
-  messagingSenderId: "1044500695902",
-  appId: "1:1044500695902:web:225777334a73b933981dcf"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
